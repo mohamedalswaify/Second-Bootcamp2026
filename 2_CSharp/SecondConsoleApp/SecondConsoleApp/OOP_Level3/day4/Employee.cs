501121858cd646fd8d52595f72646675aadda4a6
mohamedalswaify <mohamedgamal1455@gmail.com>
2026-08-03 18:42:18 +0300

    3-8-2026

---
+++ 2_CSharp/SecondConsoleApp/SecondConsoleApp/OOP_Level3/day4/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsoleApp.OOP_Level3.day4
+{
+    public abstract class Employee
+    {
+
+        //Employee 
+        //Properties Name ,Age,Salary
+        //Method DisplayEmployeeDetails() int All the properties of Employee
+        //Method CalculateAnnaulSalary() int return Salary *12  of Employee
+
+        //EmployeeBase 
+        //Properties Name ,Age,Salary,Bonus
+        //Method DisplayEmployeeDetails() int All the properties of EmployeeBase
+        //Method CalculateAnnaulSalary() int return Salary *12  of EmployeeBase
+
+        //EmployeeHour 
+        //Properties Name ,Age,Salary,Hours,HourlyRate
+        //Method DisplayEmployeeDetails() int All the properties of EmployeeHour
+        //Method CalculateAnnaulSalary() int return Salary *12  of EmployeeHour
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        private double _salary;
+        public double Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be negative.");
+
+                }
+            }
+        }
+
+
+        public Employee(string name,int age ,double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+
+        }
+
+
+
+        public virtual string DisplayEmployeeDetails()
+        {
+
+            return $"Name: {Name}, Age: {Age}, Salary: {Salary}";
+        }
+
+        public abstract double CalculateAnnualSalary();
+      
+      
+
+
+
+    }
+}
---
+++ 2_CSharp/SecondConsoleApp/SecondConsoleApp/OOP_Level3/day4/EmployeeBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsoleApp.OOP_Level3.day4
+{
+    public class EmployeeBase : Employee
+    {
+
+        public double Bonus { get; set; }
+
+        public EmployeeBase(string name, int age, double salary,double bouns) : base(name, age, salary)
+        {
+          
+            Bonus = bouns;
+        }
+
+        public override double CalculateAnnualSalary()
+        {
+            double annual = (Salary+Bonus) * 12;
+            return annual;
+        }
+
+        public override string DisplayEmployeeDetails()
+        {
+
+            return base.DisplayEmployeeDetails() +  $"Bouns {Bonus}";
+        }
+
+    }
+}
