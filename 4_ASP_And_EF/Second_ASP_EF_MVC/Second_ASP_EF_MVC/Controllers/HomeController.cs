c9e3e494d4d46a69e7d826450a331d52020126c7
mohamedalswaify <mohamedgamal1455@gmail.com>
2026-08-30 18:21:36 +0300

    First commit

---
+++ 4_ASP_And_EF/Second_ASP_EF_MVC/Second_ASP_EF_MVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Second_ASP_EF_MVC.Models;
+
+namespace Second_ASP_EF_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //public IActionResult Mohamed()
+        //{
+        //    //Array data
+        //    IList<string> list = new List<string>();
+        //    list.Add("Mohamed");
+        //    return StatusCode(304,list);
+        //}
+
+        public IActionResult Test()
+        {
+            IList<Employees> employees = new List<Employees>();
+            employees.Add(new Employees { Id=1,Name="Mohamed",Description="Test"});
+            employees.Add(new Employees { Id=2,Name="Ali",Description="Test"});
+            employees.Add(new Employees { Id=3,Name="Ahmed",Description="Test"});
+            employees.Add(new Employees { Id=4,Name="Sara",Description="Test"});
+
+            return View(employees);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 4_ASP_And_EF/Second_ASP_EF_MVC/Second_ASP_EF_MVC/Models/Employees.cs
@@ -0,0 +1,10 @@
+﻿namespace Second_ASP_EF_MVC.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
